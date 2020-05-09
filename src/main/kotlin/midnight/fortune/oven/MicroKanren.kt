a19b3262ee17493195a83573af283d564b66f78f
Jason Reich <jason@swiftorific.co.uk>
2020-05-09 20:23:21 +0000

    Fill out more

--- src/main/kotlin/midnight/fortune/oven/MicroKanren.kt
+++ src/main/kotlin/midnight/fortune/oven/MicroKanren.kt
@@ -1,5 +1,14 @@
 package midnight.fortune.oven
 
+class Var(val index: Int)
+typealias Subst = Map<Var, Term>
+
+sealed class Term {
+    data class TAtom(val value: String): Term()
+    data class TPair(val left: Term, val right: Term): Term()
+    data class TVar(val variable: Var): Term()
+}
+
 class MicroKanren(
     val state: Map<Int, Int>
 ) {
